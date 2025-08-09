f6dc1c8fd9433264faa76abca5cdd1ca64b4c470
Arjun Singh Naruka <singhnarukaarjun@gmail.com>
2025-08-09 11:43:52 +0530

    get addresses list

--- elegant_be/routes/addressRoutes.js
+++ elegant_be/routes/addressRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { addAddress, getAddresses, updateAddress, deleteAddress } = require('../controllers/addressController');
+const { addAddress, getAddresses, updateAddress, deleteAddress, getAddressesByUserId } = require('../controllers/addressController');
 const authMiddleware = require('../middlewares/authMiddleware');
+
+router.post('/addresses', authMiddleware, getAddressesByUserId);
 
 router.post('/address', authMiddleware, addAddress);
 router.get('/addresses', authMiddleware, getAddresses);
