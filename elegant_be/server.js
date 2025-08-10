88d17492f527ceae77b1be63352c3723f635e984
Arjun Singh Naruka <singhnarukaarjun@gmail.com>
2025-08-10 10:30:27 +0530

    get addresses list

--- elegant_be/server.js
+++ elegant_be/server.js
@@ -1,37 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const mysql = require('mysql2/promise');
 const swaggerAutogen = require('./swagger');
 dotenv.config();
 require('./swagger-output.json');
+require('./config/db'); // yaha se DB check ho jayega
 
 const app = require('./app');
+
 app.use(cors());
 app.use(express.json());
 
-// MySQL Connection
-let db;
-
-const connectDB = async () => {
-  try {
-    db = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    });
-    console.log('✅ Connected to MySQL Database');
-  } catch (err) {
-    console.error('❌ Database connection failed:', err.message);
-    process.exit(1);
-  }
-};
-
-
-// Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, async () => {
+app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
-  await connectDB(); // connect to DB when server starts
 });
