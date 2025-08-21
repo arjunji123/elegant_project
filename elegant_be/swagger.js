50ede2254d9acf299b81ddf873f0f661d890ce6b
Arjun Singh Naruka <singhnarukaarjun@gmail.com>
2025-08-21 22:05:39 +0530

    update swagger

--- elegant_be/swagger.js
+++ elegant_be/swagger.js
@@ -7,7 +7,7 @@ const doc = {
     description: 'Swagger documentation for Elegant BE APIs',
     version: '1.0.0',
   },
-  host: 'localhost:8080',
+  host: 'lhttps://elegantproject-production.up.railway.app',
   schemes: ['http'],
 };
 
