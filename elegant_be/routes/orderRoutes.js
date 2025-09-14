183e33fce9e6162a2eb97b4f248ebf673c9daab9
Arjun Singh Naruka <singhnarukaarjun@gmail.com>
2025-09-14 22:09:56 +0530

    update

--- elegant_be/routes/orderRoutes.js
+++ elegant_be/routes/orderRoutes.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
-const orderController = require("../controllers/orderController");
+const { createOrder, verifyPayment, getOrderById } = require('../controllers/orderController');
 
 
 // ✅ Create a new order with full details
-router.post("/orders", authMiddleware, orderController.createOrder);
+router.post("/orders", authMiddleware,createOrder);
 
 // ✅ Verify payment after Razorpay success callback/webhook
-router.post("/orders/verify", authMiddleware, orderController.verifyPayment);
+router.post("/orders/verify", authMiddleware, verifyPayment);
 
 // ✅ Get logged-in user orders
 // router.get("/orders", authMiddleware, orderController.getUserOrders);
 
 // ✅ Get single order details
-router.get("/orders/:id", authMiddleware, orderController.getOrderById);
+router.get("/orders/:id", authMiddleware, getOrderById);
 
 module.exports = router;
