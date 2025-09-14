8d7e0b3d2d26bf17f8bd52bbc8636954c668a3b6
Arjun Singh Naruka <singhnarukaarjun@gmail.com>
2025-09-14 22:07:36 +0530

    update

--- elegant_be/routes/orderRoutes.js
+++ elegant_be/routes/orderRoutes.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { addAddress, getAddresses, updateAddress, deleteAddress, getAddressesByUserId } = require('../controllers/addressController');
 const authMiddleware = require('../middlewares/authMiddleware');
 const orderController = require("../controllers/orderController");
 
-router.post('/addresses', authMiddleware, getAddressesByUserId);
 
 // ✅ Create a new order with full details
 router.post("/orders", authMiddleware, orderController.createOrder);
