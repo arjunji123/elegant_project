6544ed1b5a3301c0e405355ca346eadef93bc2db
Arjun Singh Naruka <singhnarukaarjun@gmail.com>
2025-09-15 00:28:33 +0530

    update

--- elegant_be/controllers/orderController.js
+++ elegant_be/controllers/orderController.js
@@ -232,13 +232,14 @@ exports.verifyPayment = async (req, res) => {
     const userId = req.user.id;
     const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
 
-    // Verify Razorpay Signature
-    const hmac = crypto.createHmac("sha256", process.env.RAZORPAY_KEY_SECRET);
-    hmac.update(`${razorpay_order_id}|${razorpay_payment_id}`);
-    const digest = hmac.digest("hex");
+    // 🟢 Generate Signature Server Side
+ const expectedSignature = crypto
+      .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+      .update(`${razorpay_order_id}|${razorpay_payment_id}`)
+      .digest("hex");
 
-    if (digest !== razorpay_signature) {
-      return res.status(400).json({ success:false, message:"Payment verification failed" });
+    if (expectedSignature !== razorpay_signature) {
+      return res.status(400).json({ success: false, message: "Invalid payment signature" });
     }
 
     // Update Order
