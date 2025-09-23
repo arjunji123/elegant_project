97ce17b2d9e31b66e7516ebb8997736b044ba3d5
Arjun Singh Naruka <singhnarukaarjun@gmail.com>
2025-09-24 00:34:37 +0530

    update

--- elegant_be/routes/authRoutes.js
+++ elegant_be/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, verifyOtp, resendOtp, forgotPassword, verifyForgotOtp, resetPassword } = require('../controllers/authController');
+const { register, login, verifyOtp, resendOtp, forgotPassword, verifyForgotOtp, resetPassword, verifyMobileOtp} = require('../controllers/authController');
 router.post('/register', register);
 router.post('/login', login);
 router.post('/verify-otp', verifyOtp);
@@ -8,4 +8,6 @@ router.post('/resend-otp', resendOtp);
 router.post('/forgot-password', forgotPassword);
 router.post('/verify-forgot-otp', verifyForgotOtp);
 router.post('/reset-password', resetPassword);
+router.post('/verify-mobile-otp',verifyMobileOtp);
+
 module.exports = router;
