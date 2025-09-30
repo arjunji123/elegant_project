9a59d91f5213e7fcc94937bf641ca982fa854343
Arjun Singh Naruka <singhnarukaarjun@gmail.com>
2025-09-30 11:19:28 +0530

    update

--- elegant_be/routes/orderRoutes.js
+++ elegant_be/routes/orderRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
-const { createOrder, verifyPayment, getOrders, getUserOrders } = require('../controllers/orderController');
+const { createOrder, verifyPayment, getOrders, getUserOrders, searchUserOrders } = require('../controllers/orderController');
 
 
 // ✅ Create a new order with full details
